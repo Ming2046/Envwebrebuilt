bfba574bd231e18747d2d8518e7e98be7e39bdbe
miao.zhang1 <miao.zhang1@ucalgary.ca>
2022-09-28 13:56:57 -0600

    Delete Home Four in NavBar.js

--- components/Layouts/Navbar.js
+++ components/Layouts/Navbar.js
@@ -88,12 +88,7 @@ class Navbar extends Component {
                                                         <a className="nav-link">Home Three</a>
                                                     </Link>
                                                 </li>
-                                                /*
-                                                <li className="nav-item">
-                                                    <Link href="/index4" activeClassName="active">
-                                                        <a className="nav-link">Home Four</a>
-                                                    </Link>
-                                                </li>*/
+
                                             </ul>
                                         </li>
 
