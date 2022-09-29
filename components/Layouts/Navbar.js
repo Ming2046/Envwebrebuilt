1613c60200b110998dc50269d1b7b8c2936a5ecd
miao.zhang1 <miao.zhang1@ucalgary.ca>
2022-09-29 12:54:27 -0600

    Delete unnecessary 'Home' in NavBar

--- components/Layouts/Navbar.js
+++ components/Layouts/Navbar.js
@@ -62,12 +62,7 @@ class Navbar extends Component {
                                 </button>
 
                                 <div className={classOne} id="navbarSupportedContent">
-                                    <ul className="navbar-nav">
-                                        <li className="nav-item">
-                                            <Link href="/#" activeClassName="active">
-                                                <a className="nav-link">Home</a>
-                                            </Link>
-                                        </li>
+
 
                                         <li className="nav-item">
                                             <Link href="/about-us">
