441f1df2ad0323d48d7f15f8c13bc41bb4cf29cd
miao.zhang1 <miao.zhang1@ucalgary.ca>
2022-10-07 12:46:23 -0600

    Delete text

--- components/HomeThree/Projects.js
+++ components/HomeThree/Projects.js
@@ -21,7 +21,7 @@ class Projects extends Component {
                                 </div>
             
                                 <div className="image-hover">
-                                    <div className="text stretched-link">
+                                    <div className="stretched-link">
                                         <h3>
                                             <Link href="/project-details">
                                                 <a className="stretched-link">Data is the new science big data holds the answer</a>
