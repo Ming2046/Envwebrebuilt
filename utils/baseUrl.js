451ce27778d7b6e303978e5274943ad61225e71c
miao.zhang1 <miao.zhang1@ucalgary.ca>
2022-10-08 22:16:16 -0600

    Try to make contact form works

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,5 +1,5 @@
 const baseUrl = process.env.NODE_ENV === "production" 
-? 'https://api.sendgrid.com'
-: 'https://fishcreekpark.homes';
+? 'https://fishcreekpark.homes'
+: 'https://localhost:3000';
 
 export default baseUrl;
