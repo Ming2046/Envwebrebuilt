7c39d83ce54438d6b012125bce393dcdd28c77a0
miao.zhang1 <miao.zhang1@ucalgary.ca>
2022-10-09 20:42:59 -0600

    Rebuilt contact form

--- components/Contact/freecontact.js
+++ components/Contact/freecontact.js
@@ -2,20 +2,34 @@
 // For more help visit https://formspr.ee/react-help
 import React, {useState} from 'react';
 import { useForm, ValidationError } from '@formspree/react';
-function ContactForm() {
-    const [state, handleSubmit] = useForm("xqkjlkev");
-    if (state.succeeded) {
-        return <p>Thanks for joining!</p>;
-    }
+//import baseUrl from "../../utils/baseUrl";
+//import axios from "axios";
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+const MySwal = withReactContent(Swal)
+const [state, handleSubmit] = useForm("xqkjlkev");
+const alertContent = () => {
+    MySwal.fire({
+        title: 'Congratulations!',
+        text: 'Your message was successfully send and will back to you soon',
+        icon: 'success',
+        timer: 2000,
+        timerProgressBar: true,
+        showConfirmButton: false,
+    })
+}
 
-    // Form initial state
-    const INITIAL_STATE = {
-        name: "",
-        email: "",
-        number: "",
-        subject: "",
-        text: ""
-    };
+// Form initial state
+const INITIAL_STATE = {
+    name: "",
+    email: "",
+    number: "",
+    subject: "",
+    text: ""
+};
+
+const ContactForm = () => {
+
     const [contact, setContact] = useState(INITIAL_STATE);
 
     const handleChange = e => {
@@ -23,6 +37,22 @@ function ContactForm() {
         setContact(prevState => ({ ...prevState, [name]: value }));
         // console.log(contact)
     }
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        try {
+            //const url = `${baseUrl}/api/contact`;
+            const { name, email, number, subject, text } = contact;
+            //const payload = { name, email, number, subject, text };
+            //const response = await axios.post(url, payload);
+            //console.log(response);
+            setContact(INITIAL_STATE);
+            alertContent();
+        } catch (error) {
+            console.log(error)
+        }
+    };
+
     return (
         <div className="contact-section ptb-100">
             <div className="container">
@@ -115,11 +145,7 @@ function ContactForm() {
                 </div>
             </div>
         </div>
-    );
+    )
 }
-function App() {
-    return (
-        <ContactForm />
-    );
-}
-export default App;
+
+export default ContactForm;
