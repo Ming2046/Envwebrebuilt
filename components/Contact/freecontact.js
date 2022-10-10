0617423c5e0bc10ae87ab5c27591ef9409c9c328
miao.zhang1 <miao.zhang1@ucalgary.ca>
2022-10-09 21:39:21 -0600

    Modify succeed icon and reset form

--- components/Contact/freecontact.js
+++ components/Contact/freecontact.js
@@ -1,11 +1,43 @@
 // Make sure to run npm install @formspree/react
 // For more help visit https://formspr.ee/react-help
-import React from 'react';
+import React, {useState} from 'react';
 import { useForm, ValidationError } from '@formspree/react';
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+const MySwal = withReactContent(Swal)
+
 function ContactForm() {
     const [state, handleSubmit] = useForm("xqkjlkev");
+    const alertContent = () => {
+        MySwal.fire({
+            title: 'Congratulations!',
+            text: 'Your message was successfully send and will back to you soon',
+            icon: 'success',
+            timer: 2000,
+            timerProgressBar: true,
+            showConfirmButton: false,
+        })
+    }
+
+// Form initial state
+    const INITIAL_STATE = {
+        name: "",
+        email: "",
+        number: "",
+        subject: "",
+        text: ""
+    };
+    const [contact, setContact] = useState(INITIAL_STATE);
+
+    const handleChange = e => {
+        const { name, value } = e.target;
+        setContact(prevState => ({ ...prevState, [name]: value }));
+        // console.log(contact)
+    }
+
     if (state.succeeded) {
-        return <p>Thanks for joining!</p>;
+        setContact(INITIAL_STATE);
+        alertContent();
     }
     return (
         <div className="contact-section ptb-100">
