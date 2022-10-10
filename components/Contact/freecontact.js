8874351cbf3e539d025ba8d3f574478a3b2ba809
miao.zhang1 <miao.zhang1@ucalgary.ca>
2022-10-09 21:20:09 -0600

    Rebuilt contact form

--- components/Contact/freecontact.js
+++ components/Contact/freecontact.js
@@ -8,33 +8,67 @@ function ContactForm() {
         return <p>Thanks for joining!</p>;
     }
     return (
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="email">
-                Email Address
-            </label>
-            <input
-                id="email"
-                type="email"
-                name="email"
-            />
-            <ValidationError
-                prefix="Email"
-                field="email"
-                errors={state.errors}
-            />
-            <textarea
-                id="message"
-                name="message"
-            />
-            <ValidationError
-                prefix="Message"
-                field="message"
-                errors={state.errors}
-            />
-            <button type="submit" disabled={state.submitting}>
-                Submit
-            </button>
-        </form>
+        <div className="contact-section ptb-100">
+            <div className="container">
+                <div className="row align-items-center">
+                    <div className="col-lg-6">
+                        <div className="contact-image">
+                            <img src="/images/contact.png" alt="image" />
+                        </div>
+                    </div>
+
+                    <div className="col-lg-6">
+                        <div className="contact-form">
+                            <form onSubmit={handleSubmit}>
+                                    <div className="col-lg-6 col-md-6">
+                                        <div className="form-group">
+                                            <input
+                                                type="text"
+                                                name="email"
+                                                placeholder="Email"
+                                                className="form-control"
+                                                value={contact.email}
+                                                onChange={handleChange}
+                                                required
+                                            />
+                                            <ValidationError
+                                                prefix="Email"
+                                                field="email"
+                                                errors={state.errors}
+                                            />
+                                        </div>
+                                    </div>
+
+                                    <div className="col-lg-12 col-md-12">
+                                        <div className="form-group">
+                                            <textarea
+                                                name="text"
+                                                cols="30"
+                                                rows="6"
+                                                placeholder="Write your message..."
+                                                className="form-control"
+                                                value={contact.text}
+                                                onChange={handleChange}
+                                                required
+                                            />
+                                            <ValidationError
+                                                prefix="Message"
+                                                field="message"
+                                                errors={state.errors}
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className="col-lg-12 col-sm-12">
+                                        <button type="submit" className="submit-btn mt-2">
+                                            Send Message!
+                                        </button>
+                                    </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 function App() {
