3c494a843edef1934b1a7b7c55c13836fd961f54
miao.zhang1 <miao.zhang1@ucalgary.ca>
2022-10-10 20:19:21 -0600

    Show when contact us successes

--- components/Contact/freecontact.js
+++ components/Contact/freecontact.js
@@ -13,10 +13,64 @@ function ContactForm() {
             <div className="contact-section ptb-100">
                 <div className="container">
                     <div className="row align-items-center">
-                        <p>Thanks for contacting us!</p>;
+                        <div className="col-lg-6">
+                            <div className="contact-image">
+                                <img src="/images/contact.png" alt="image" />
+                            </div>
+                        </div>
+
+                        <div className="col-lg-6">
+                            <div className="contact-form">
+                                <form onSubmit={handleSubmit}>
+                                    <div className="col-lg-12 col-md-12">
+                                        <div className="form-group">
+                                            <input
+                                                type="text"
+                                                name="email"
+                                                placeholder="Email"
+                                                className="form-control"
+                                                required
+                                            />
+                                            <ValidationError
+                                                prefix="Email"
+                                                field="email"
+                                                errors={state.errors}
+                                            />
+                                        </div>
+                                    </div>
+
+                                    <div className="col-lg-12 col-md-12">
+                                        <div className="form-group">
+                                            <textarea
+                                                name="text"
+                                                cols="30"
+                                                rows="6"
+                                                placeholder="Write your message..."
+                                                className="form-control"
+
+                                                required
+                                            />
+                                            <ValidationError
+                                                prefix="Message"
+                                                field="message"
+                                                errors={state.errors}
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className="col-lg-12 col-sm-12">
+                                        <button type="submit" className="submit-btn mt-2">
+                                            Send Message!
+                                        </button>
+                                        <p>Thank you for contacting us!</p>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
+
+
         )
     }
     return (
