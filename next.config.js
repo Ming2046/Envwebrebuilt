bbe18a4ebe33b40a6711af670b0e33f888d4a4fe
miao.zhang1 <miao.zhang1@ucalgary.ca>
2022-10-14 21:31:43 -0600

    Plan to use SVGR for svg file, add it to package file nd next configure file

--- next.config.js
+++ next.config.js
@@ -4,6 +4,15 @@ module.exports = {
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
     },
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/i,
+            issuer: /\.[jt]sx?$/,
+            use: ['@svgr/webpack'],
+        })
+
+        return config
+    },
     trailingSlash: true,
     optimizeFonts: false,
     distDir: 'build',
